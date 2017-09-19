17637d8e8b13e847254e5ca66bf23393cf63f988
James Blocker and Kyle Ebersole <james@>
2017-09-19 10:59:56 -0700

    Create project directory. Add html and js to collect checkbox values and output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,17 @@
-var inputString = "Believe you can and you're halfway there. Theodore Roosevelt";
-var outputString = "";
+$(document).ready(function(){
+$("form#transportation_survey").submit(function(event){
+  event.preventDefault();
+  $("#work-responses").show();
+  $("input:checkbox[name=work-transportation]:checked").each(function(){
+    var workTransportationMode = $(this).val();
+    $("#work-responses").append(workTransportationMode + "<br>");
+    });
 
+  $("input:checkbox[name=soda-number]:checked").each(function(){
+    var sodaNumberInput = $(this).val();
+    $("#soda-responses").text(sodaNumberInput);
+  });
 
-var splitString = inputString.split("");
-for (i = 0; i <= splitString.length; i++) {
-  var currentChar = inputString[i];
-  if (splitString[i] === "a" ||
-    splitString[i] === "e" ||
-    splitString[i] === "i" ||
-    splitString[i] === "o" ||
-    splitString[i] === "u") {
-    currentChar = "-";
-  }
-  outputString = outputString + currentChar;
-}
-console.log(outputString);
-
-//|| splitString[i] = "e" || splitString[i] = "i" || splitString[i] = "o" || splitString[i] = "u")
-
-// if () is a) {
-//   string.replace(a,-);
-// }else if (is e) {
-//   string.replace()
-// }
-
-// inputstring.replace(a,- 'g')
-// input
+    $("#transportation_survey").hide();
+  });
+});
