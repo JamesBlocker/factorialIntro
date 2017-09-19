a3a925eb36b67729d8af07df82604ce38c5539d4
James Blocker and Kyle Ebersole <james@>
2017-09-19 13:03:06 -0700

    Add js script for questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
-$("form#transportation_survey").submit(function(event){
+$("form#anxiety-quiz").submit(function(event){
   event.preventDefault();
-  $("#work-responses").show();
-  $("input:checkbox[name=work-transportation]:checked").each(function(){
-    var workTransportationMode = $(this).val();
-    $("#work-responses").append(workTransportationMode + "<br>");
+  $("#questionOneResponse").show();
+  $("input:checkbox[name=social-question]:checked").each(function(){
+    var q1Answer = $(this).val();
+    $("#questionOneResponse").append(q1Answer + "<br>");
     });
 
-  $("input:checkbox[name=soda-number]:checked").each(function(){
-    var sodaNumberInput = $(this).val();
-    $("#soda-responses").text(sodaNumberInput);
+  $("#questionTwoResponse").show();
+  $("input:checkbox[name=deadline-question]:checked").each(function(){
+    var q2Answer = $(this).val();
+    $("#questionTwoResponse").append(q2Answer + "<br>");
+  });
+
+  $("#questionThreeResponse").show();
+  $("input:checkbox[name=bed-question]:checked").each(function(){
+    var q3Answer = $(this).val();
+    $("#questionThreeResponse").append(q3Answer + "<br>");
+  });
+
+  $("#questionFourResponse").show();
+  $("input:checkbox[name=pair-question]:checked").each(function(){
+    var q4Answer = $(this).val();
+    $("#questionFourResponse").append(q4Answer + "<br>");
   });
 
     $("#transportation_survey").hide();
