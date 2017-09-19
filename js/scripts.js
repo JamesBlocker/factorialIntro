9268458020f880ae21f42e3a1cb09d4bc4047d1e
James Blocker and Kyle Ebersole <james@>
2017-09-19 16:07:22 -0700

    Create project. Add js and html to display factorial.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,24 @@
+var userNumber;
+var numberArray = [];
+var total = 1;
+
 $(document).ready(function(){
-$("form#anxiety-quiz").submit(function(event){
+
+  $("form#factorial").submit(function(event){
   event.preventDefault();
-  $("#questionOneResponse").show();
-  $("input:checkbox[name=social-question]:checked").each(function(){
-    var q1Answer = $(this).val();
-    $("#questionOneResponse").append(q1Answer + "<br>");
-    });
+  $("#results").show();
+  $("form#factorial").hide();
+  userNumber = parseInt($("input#userInput").val());
 
-  $("#questionTwoResponse").show();
-  $("input:checkbox[name=deadline-question]:checked").each(function(){
-    var q2Answer = $(this).val();
-    $("#questionTwoResponse").append(q2Answer + "<br>");
+  for (i = userNumber; i >= 1; i--) {
+    numberArray.push(i);
+  }
+
+  numberArray.forEach(function(number){
+    total = total * number;
   });
 
-  $("#questionThreeResponse").show();
-  $("input:checkbox[name=bed-question]:checked").each(function(){
-    var q3Answer = $(this).val();
-    $("#questionThreeResponse").append(q3Answer + "<br>");
+  $("#results").text(total);
   });
 
-  $("#questionFourResponse").show();
-  $("input:checkbox[name=pair-question]:checked").each(function(){
-    var q4Answer = $(this).val();
-    $("#questionFourResponse").append(q4Answer + "<br>");
-  });
-
-    $("#transportation_survey").hide();
-  });
 });
